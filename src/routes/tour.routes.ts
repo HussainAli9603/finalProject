5073499459fa7a10df84f64e990ba7706ccf9490
HussainAli9603 <72409657+HussainAli9603@users.noreply.github.com>
2021-09-13 07:24:52 -0700

    first commit

--- src/routes/tour.routes.ts
+++ src/routes/tour.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { TourController } from '../controller/tour.controller';
-import { ITourResponse, IGetTourResponse, IUpdateTourResponse, IAllTourResponse } from '../types/responses/tour.responses';
-import { ITourRequest, IGetTour, IUpdateTourRequest, IDeleteTour } from '../types/requests/tour.request';
+import { ITourResponse } from '../types/responses/tour.responses';
+import { ITourRequest } from '../types/requests/tour.request';
 
 
 export class TourRoutes {
@@ -11,21 +11,20 @@ export class TourRoutes {
         this.routes();
     }
     routes() {
-
-        //Adding Tour Route
-        this.router.post('/addtour', async (req: any, res, next) => {
+        // Register new tour ..route
+        this.router.post('/addtour', async (req, res, next) => {
             try {
-                var buss = await new TourController().saveTour(req.body);
-                res.status(200).json({ addbuss: buss }).end()
+                var tour = await new TourController().saveTour(req.body);
+                res.status(200).json(
+                    {
+                        Tourdetails: tour
+                    }
+                ).end()
             } catch (error) {
                 next(error);
             }
         });
-
-
-
     }
-
 }
 
 export const TourRoutesApi = new TourRoutes().router;
