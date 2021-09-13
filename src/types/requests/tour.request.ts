909a00c8573710015965335a1c01122cfcb55f36
HussainAli9603 <72409657+HussainAli9603@users.noreply.github.com>
2021-09-13 07:25:57 -0700

    first commit

--- src/types/requests/tour.request.ts
+++ src/types/requests/tour.request.ts
@@ -3,19 +3,3 @@ export interface ITourRequest {
     Date: "yyyy-mm-dd";
     Participants: number
 }
-
-export interface IGetTour {
-    _id: string;
-}
-
-export interface IUpdateTourRequest {
-    _id: string;
-    participants: string;
-    bookingDate: string;
-    destination: string;
-    person: string;
-}
-
-export interface IDeleteTour {
-    _id: string;
-}
